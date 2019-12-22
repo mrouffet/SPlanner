78d0a121ac52471532c21904bd005863c14d0df9
mrouffet <maximerouffet@gmail.com>
2019-12-22 18:13:42 +0100

    [Add] SP_Goal

---
+++ Plugins/SPlanner/Source/SPlanner/Public/Goal/Goal.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <Engine/DataAsset.h>
+#include "Goal.generated.h"
+
+/**
+ *	Planner goal asset.
+ */
+UCLASS(BlueprintType, Blueprintable, ClassGroup = "SPlanner|Goal")
+class SPLANNER_API USP_Goal : public UDataAsset
+{
+	GENERATED_BODY()
+
+};
