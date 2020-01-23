58b4a1dc5dd0d7f66eab90df778a23abcbbdeb04
mrouffet <maximerouffet@gmail.com>
2020-01-23 11:00:15 +0100

    [Update] SP_Debug blueprint

--- Plugins/SPlanner/Source/Public/SPlanner/Debug/SP_Debug.h
+++ Plugins/SPlanner/Source/Public/SPlanner/Debug/SP_Debug.h
@@ -7,26 +7,26 @@
 #include "SP_Debug.generated.h"
 
 UCLASS(Abstract, ClassGroup = "SPlanner|Debug")
-class USP_Debug : public UObject
+class SPLANNER_API USP_Debug : public UObject
 {
 	GENERATED_BODY()
 
 public:
 	/** Blueprint accessor to SP_LOG */
-	UFUNCTION(BlueprintCallable, Category = "SPlanner|Debug")
+	UFUNCTION(BlueprintCallable, DisplayName = "SP_Log", Category = "SPlanner|Debug")
 	static void Log(UObject* Caller, const FString& Str = "Hello", ESP_DebugVerbosity Verbosity = ESP_DebugVerbosity::DV_Display);
 
 	/**
 	*	Blueprint accessor to SP_LOG_SCREEN.
 	*	Use Verbosity color.
 	*/
-	UFUNCTION(BlueprintCallable, Category = "SPlanner|Debug")
+	UFUNCTION(BlueprintCallable, DisplayName = "SP_LogScreen", Category = "SPlanner|Debug")
 	static void LogScreen(UObject* Caller, const FString& Str = "Hello", ESP_DebugVerbosity Verbosity = ESP_DebugVerbosity::DV_Display);
 
 	/**
 	*	Blueprint accessor to SP_LOG_SCREEN.
 	*	Use custom color.
 	*/
-	UFUNCTION(BlueprintCallable, Category = "SPlanner|Debug")
+	UFUNCTION(BlueprintCallable, DisplayName = "SP_LogScreenFull", Category = "SPlanner|Debug")
 	static void LogScreenFull(UObject* Caller, const FString& Str = "Hello", ESP_DebugVerbosity Verbosity = ESP_DebugVerbosity::DV_Display, FLinearColor Color = FLinearColor::Yellow, float Time = 5.0f);
 };
