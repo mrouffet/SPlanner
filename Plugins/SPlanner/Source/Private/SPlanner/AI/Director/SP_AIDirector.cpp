681edd51341dcdfa637236498c9b0fef86f16d82
mrouffet <maximerouffet@gmail.com>
2020-02-01 12:02:55 +0100

    [Add] AIDirector callback AI num change

--- Plugins/SPlanner/Source/Private/SPlanner/AI/Director/SP_AIDirector.cpp
+++ Plugins/SPlanner/Source/Private/SPlanner/AI/Director/SP_AIDirector.cpp
@@ -1,2 +1,14 @@
 #include <SPlanner/AI/Director/SP_AIDirector.h>
 
+void ASP_AIDirector::OnRegistedPlannerActive(USP_PlannerComponent* InPlanner)
+{
+	Super::OnRegistedPlannerActive(InPlanner);
+
+	OnAINumChange.Broadcast(GetPlannerNum());
+}
+void ASP_AIDirector::OnRegistedPlannerInactive(USP_PlannerComponent* InPlanner)
+{
+	Super::OnRegistedPlannerInactive(InPlanner);
+
+	OnAINumChange.Broadcast(GetPlannerNum());
+}
--- Plugins/SPlanner/Source/Public/SPlanner/AI/Director/SP_AIDirector.h
+++ Plugins/SPlanner/Source/Public/SPlanner/AI/Director/SP_AIDirector.h
@@ -3,8 +3,19 @@
 #include <SPlanner/Base/Director/SP_Director.h>
 #include "SP_AIDirector.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FSP_AIDirectorNumDelegate, int, AINum);
+
 UCLASS(BlueprintType, Blueprintable, ClassGroup = "SPlanner|Director")
 class SPLANNER_API ASP_AIDirector : public ASP_Director
 {
 	GENERATED_BODY()
+
+protected:
+	void OnRegistedPlannerActive(USP_PlannerComponent* InPlanner) override;
+	void OnRegistedPlannerInactive(USP_PlannerComponent* InPlanner) override;
+
+public:
+	/** Callback event when AI get active or inactive. */
+	UPROPERTY(BlueprintAssignable, Category = "SPlanner|Director|AI")
+	FSP_AIDirectorNumDelegate OnAINumChange;
 };
