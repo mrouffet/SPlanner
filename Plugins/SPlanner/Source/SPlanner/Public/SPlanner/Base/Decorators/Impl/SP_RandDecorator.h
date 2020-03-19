806f4ebc56f534174df27925a1084b69d8873c6f
mrouffet <maximerouffet@gmail.com>
2020-03-19 16:08:59 +0100

    [Add] SP_RandDecorator

---
+++ Plugins/SPlanner/Source/SPlanner/Public/SPlanner/Base/Decorators/Impl/SP_RandDecorator.h
@@ -0,0 +1,26 @@
+// Copyright 2020 Maxime ROUFFET. All Rights Reserved.
+
+#pragma once
+
+#include <SPlanner/Base/Decorators/SP_Decorator.h>
+#include "SP_RandDecorator.generated.h"
+
+/**
+ *	SPlanner Random success rate decorator.
+ */
+UCLASS(BlueprintType, Blueprintable, EditInlineNew, ClassGroup = "SPlanner|Decorator")
+class SPLANNER_API USP_RandDecorator : public USP_Decorator
+{
+	GENERATED_BODY()
+	
+protected:
+	/**
+	*	Success rate of random.
+	*	Rand <= SuccessRate: Success.
+	*	Rand > SuccessRate: Failure.
+	*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SPlanner|Random")
+	float SuccessRate = 0.5f;
+
+	bool Validate_Internal_Implementation(const USP_PlannerComponent* Planner) override;
+};
