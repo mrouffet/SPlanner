9788cfd8ee2963ec415e0fb8cf8081383c97e5b5
mrouffet <maximerouffet@gmail.com>
2021-11-11 11:27:11 +0100

    [Add] Empty GOAP planner impl

---
+++ Include/SPlanner/GOAP/Base/AGOAPPlanner.hpp
@@ -0,0 +1,17 @@
+// Copyright(c) 2021 Maxime "mrouffet" ROUFFET. All Rights Reserved.
+
+#pragma once
+
+#ifndef SPLANNER_GOAP_AGOAP_PLANNER_GUARD
+#define SPLANNER_GOAP_AGOAP_PLANNER_GUARD
+
+#include <SPlanner/Abstract/APlanner.hpp>
+
+namespace SP
+{
+	class AGOAPPlanner : public APlanner
+	{
+	};
+}
+
+#endif // GUARD
---
+++ Include/SPlanner/GOAP/Base/GOAPLinearPlanner.hpp
@@ -0,0 +1,17 @@
+// Copyright(c) 2021 Maxime "mrouffet" ROUFFET. All Rights Reserved.
+
+#pragma once
+
+#ifndef SPLANNER_GOAP_GOAP_LINEAR_PLANNER_GUARD
+#define SPLANNER_GOAP_GOAP_LINEAR_PLANNER_GUARD
+
+#include <SPlanner/GOAP/Base/AGOAPPlanner.hpp>
+
+namespace SP
+{
+	class GOAPLinearPlanner : public AGOAPPlanner
+	{
+	};
+}
+
+#endif // GUARD
