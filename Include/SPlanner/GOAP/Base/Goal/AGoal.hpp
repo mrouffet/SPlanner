6dd77d2b56bcdac0a78b172047c2e766476c5dee
mrouffet <maximerouffet@gmail.com>
2021-11-11 11:27:34 +0100

    [Add] GOAP Abstract Goal

---
+++ Include/SPlanner/GOAP/Base/Goal/AGoal.hpp
@@ -0,0 +1,15 @@
+// Copyright(c) 2021 Maxime "mrouffet" ROUFFET. All Rights Reserved.
+
+#pragma once
+
+#ifndef SPLANNER_GOAP_AGOAL_GUARD
+#define SPLANNER_GOAP_AGOAL_GUARD
+
+namespace SP
+{
+	class AGoal
+	{
+	};
+}
+
+#endif // GUARD
