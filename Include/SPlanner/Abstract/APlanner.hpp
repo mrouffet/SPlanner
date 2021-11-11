8987bfe0066599d8d1b64ed03d9d8a14acca7469
mrouffet <maximerouffet@gmail.com>
2021-11-11 11:26:46 +0100

    [Add] Abstract Plan & Step

---
+++ Include/SPlanner/Abstract/APlanner.hpp
@@ -0,0 +1,15 @@
+// Copyright(c) 2021 Maxime "mrouffet" ROUFFET. All Rights Reserved.
+
+#pragma once
+
+#ifndef SPLANNER_ABSTRACT_APLANNER_GUARD
+#define SPLANNER_ABSTRACT_APLANNER_GUARD
+
+namespace SP
+{
+	class APlanner
+	{
+	};
+}
+
+#endif // GUARD
---
+++ Include/SPlanner/Abstract/AStep.hpp
@@ -0,0 +1,15 @@
+// Copyright(c) 2021 Maxime "mrouffet" ROUFFET. All Rights Reserved.
+
+#pragma once
+
+#ifndef SPLANNER_ABSTRACT_ASTEP_GUARD
+#define SPLANNER_ABSTRACT_ASTEP_GUARD
+
+namespace SP
+{
+	class AStep
+	{
+	};
+}
+
+#endif // GUARD
