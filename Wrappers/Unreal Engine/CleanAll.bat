1b569e7f2ff934844b7a7768696e0ef799e5f284
mrouffet <maximerouffet@gmail.com>
2021-11-12 15:18:23 +0100

    [Add] UE 4.27 demo project
    Add demo project required environment for plugin development

---
+++ Wrappers/Unreal Engine/CleanAll.bat
@@ -0,0 +1,20 @@
+:: Copyright (c) 2021 Maxime "mrouffet" ROUFFET. All Rights Reserved.
+
+:: Kill Unreal Engine and visual studio process
+TASKKILL /f /im UE4Editor.exe
+TASKKILL /f /im devenv.exe
+
+::Wait for kills.
+TIMEOUT 3 /nobreak
+
+:: Clean files
+RMDIR /s /q .vs
+RMDIR /s /q Binaries
+RMDIR /s /q Intermediate
+RMDIR /s /q Saved
+RMDIR /s /q DerivedDataCache
+
+RMDIR /s /q Plugins\SPlanner\Binaries
+RMDIR /s /q Plugins\SPlanner\Intermediate
+
+DEL /s /q *.sln
---
+++ Wrappers/Unreal Engine/Source/SPlannerDemo/SPlannerDemoGameMode.h
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SPlannerDemoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASPlannerDemoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASPlannerDemoGameMode();
+};
